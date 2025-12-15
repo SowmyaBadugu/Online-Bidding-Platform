6835d4bebaad6b195996ef3044127072a9463abb
SowmyaBadugu <2200031010@kluniversity.in>
2025-12-15 08:12:56 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SESSION_COOKIE_HTTPONLY'] = True
 DB_CONFIG = {
     'host': os.environ.get('DB_HOST', 'localhost'),
     'user': os.environ.get('DB_USER', 'root'),
-    'password': os.environ.get('DB_PASSWORD', 'your_password'),
+    'password': os.environ.get('DB_PASSWORD', 'sowmya2004'),
     'database': os.environ.get('DB_NAME', 'bidding_system'),
     'port': int(os.environ.get('DB_PORT', 3306))
 }
